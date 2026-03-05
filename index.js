bf22f41de26c94ce2d33ab528c066d82c9c219e1
Shabaab Hanaan <shababhanaan22@gmail.com>
2026-03-05 09:51:22 +0530

    Add working JS GitHub Action for Hanaan

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const core = require('@actions/core');
 
-try{
-    //Input
-    const name = core.getInput('name');
+try {
+  // Get input from workflow
+  const name = core.getInput('name');
 
-    const greeting = `Hello, ${name}`; 
+  // Generate greeting
+  const greeting = `Hello, ${name}!`;
 
-    console.log(greeting);
+  console.log(greeting);
 
-    core.setOutput('greeting', greeting);
-}catch(error){
-    core.setFailed(error.message);
+  // Set output for future steps
+  core.setOutput('greeting', greeting);
+
+} catch (error) {
+  core.setFailed(error.message);
 }
